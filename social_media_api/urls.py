3614535c11fd0bef975c26a8d3ed73998e5e3210
Alexander <sasacihun@gmail.com>
2024-07-02 12:37:46 +0300

    Black formatter

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/v1/social_media/", include("social_media.urls", namespace="social_media")),
+    path("admin/", admin.site.urls),
+    path(
+        "api/v1/social_media/", include("social_media.urls", namespace="social_media")
+    ),
+    path("api/v1/user/", include("user.urls", namespace="user")),
 ]
