926bb8c8e017c28916590b10672ee4008894e696
Alexander <sasacihun@gmail.com>
2024-07-07 13:39:19 +0300

    fix: Add atribute check for follow page (Follow model haven't user field)

--- social_media/permissions.py
+++ social_media/permissions.py
@@ -5,10 +5,15 @@ from rest_framework.permissions import BasePermission
 class IsAuthorOrReadOnly(BasePermission):
     """
     Read : anyone
-    Create, Update, Delete : author only
+    Delete : admin only
+    Create, Update : author only
     """
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+
+        if hasattr(obj, "follower"):
+            return obj.follower == request.user
+
         return obj.user == request.user
