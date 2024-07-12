ee4a29b9c3cc97be886b5f55b8a061a2cd4d393e
Alexander <sasacihun@gmail.com>
2024-07-12 14:34:42 +0300

    fix: Add basename routers, change name for paths in urlpatterns for `reverse()` usage

--- social_media/urls.py
+++ social_media/urls.py
@@ -20,10 +20,10 @@ from social_media.views import (
 app_name = "social_media"
 
 router = routers.DefaultRouter()
-router.register("posts", PostViewSet)
+router.register("posts", PostViewSet, basename="posts")
 
 my_profile_router = routers.DefaultRouter()
-my_profile_router.register("user-posts", UserPostsViewSet)
+my_profile_router.register("user-posts", UserPostsViewSet, basename="user-posts")
 my_profile_router.register("user-comments", CommentViewSet)
 my_profile_router.register(
     "user-following", MyProfileFollowingViewSet, basename="user-following"
@@ -36,30 +36,31 @@ profile_router = routers.DefaultRouter()
 profile_router.register("following", FollowingViewSet, basename="user-following")
 profile_router.register("followers", FollowersViewSet, basename="user-followers")
 
+
 urlpatterns = [
     path("", include(router.urls)),
-    path("my-profile/", RetrieveProfileAPIView.as_view()),
+    path("my-profile/", RetrieveProfileAPIView.as_view(), name="my-profile"),
     path("my-profile/<int:user_id>/", UpdateProfileAPIView.as_view()),
     path("my-profile/", include(my_profile_router.urls)),
     path(
         "my-profile/user-posts/<int:post_id>/add_comment/",
         AddCommentAPIView.as_view(),
-        name="add-comment",
+        name="add-comment-to-me",
     ),
     path(
         "posts/<int:post_id>/add_comment/",
         AddCommentAPIView.as_view(),
-        name="add-comment",
+        name="add-comment-to-sb",
     ),
     path(
         "posts/<int:post_id>/like/",
         ToggleLikeAPIView.as_view(),
-        name="add-like",
+        name="toggle-like-for-sb",
     ),
     path(
         "my-profile/user-posts/<int:post_id>/like/",
         ToggleLikeAPIView.as_view(),
-        name="add-like",
+        name="toggle-like-for-me",
     ),
     path(
         "profile/<str:username>/",
